cdb365beb149cd2d7bb7995c23a75d16269dd6c7
Swati Yadawar <r.swati2211@gmail.com>
2024-03-08 11:24:33 +0530

    first commit

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import dz, {a,c,d} from './module2.mjs'
+console.log(dz);
+console.log(c);
+console.log(d);
+console.log(a);
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a="Harry";
+const b="Rohan";
+const c="Swati";
+const d="Atharva";
+
+export default b;
+export {a};
+export {c};
+export {d};
